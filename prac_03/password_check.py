d55eb8b59853d68acb7663b17b261450185247fd
ethan <ethanbarr64@gmail.com>
2020-08-13 12:35:24 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,16 +1,33 @@
 """Password check Program
-checks user input length and and print * password if valid"""
+checks user input length and and print * password if valid, BUT with functions"""
 
-valid = False
-while not valid:
-    password = input("Please enter password: ")
-    password_count = 0
+minimum_length = 3
+
+def main():
+    password = get_password(minimum_length)
+    convert_password(password)
+
+
+def convert_password(password):
+    """converts password input to an equal length "*" output"""
     for char in password:
-        password_count += 1
-    if password_count <= 3 or password_count >= 10:
-        print("invalid password")
-    else:
-        print("valid password")
-        valid = True
-for char in password:
-    print("*", end="")
+        print("*", end="")
+
+
+def get_password(minimum_length):
+    """takes a users input and checks that it is greater than the minimum length
+    if not, repeats till valid then returns the password"""
+    valid = False
+    while not valid:
+        password = input("Please enter password greater than 3 characters long: ")
+        password_count = 0
+        for char in password:
+            password_count += 1
+        if password_count <= minimum_length:
+            print("invalid password")
+        else:
+            print("valid password")
+            valid = True
+    return password
+
+main()
