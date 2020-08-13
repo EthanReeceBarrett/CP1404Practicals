654f1614f1fec9c86adc9e464ac5e54753fabdf5
ethan <ethanbarr64@gmail.com>
2020-08-13 13:00:12 +1000

    Refactor broken score program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,18 +1,45 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
+Broken program to determine score status, now with functions.
 """
+import random
 
 # fixed
+def main():
+    """runs user score and user random score functions and prints the results"""
+    score = user_score()
+    print("score is {}".format(score))
+    user_random_score = random_score()
+    print("random score is {}".format(user_random_score))
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-else:
-    print("Bad")
+def user_score():
+    """takes score input and returns result"""
+    score = float(input("Enter score: "))
+    if score < 0:
+        result = "Invalid"
+    elif score > 100:
+        result = "Invalid"
+    elif score > 90:
+        result = "Excellent"
+    elif score > 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+def random_score():
+    """generates random input and returns result"""
+    random_score = random.randint(0,100)
+    if random_score < 0:
+        result = "Invalid"
+    elif random_score > 100:
+        result = "Invalid"
+    elif random_score > 90:
+        result = "Excellent"
+    elif random_score > 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+main()
