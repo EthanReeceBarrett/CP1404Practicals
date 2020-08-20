0ea367a310af13c635853aa4e5576e252392148a
ethan <ethanbarr64@gmail.com>
2020-08-20 16:05:40 +1000

    refactor quick_picks.py to print random numbers to a list

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,3 +6,20 @@ import random
 NUMBERS_PER_LINE = 6
 MAX_NUMBER = 45
 MIN_NUMBER = 1
+
+
+def main():
+    number_of_lines = int(input("how many lines? "))
+    while number_of_lines < 1:
+        print("invald amount of lines")
+        number_of_lines = int(input("how many lines? "))
+    for number in range(number_of_lines):
+        quick_picks = []
+        for number_2 in range(NUMBERS_PER_LINE):
+            random_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            quick_picks.append(random_number)
+        quick_picks.sort()
+        print(quick_picks)
+
+
+main()
