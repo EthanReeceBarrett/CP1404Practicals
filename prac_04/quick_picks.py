12725ea2682639f7fa08bf7c02faf4ee6026465a
ethan <ethanbarr64@gmail.com>
2020-08-20 15:52:25 +1000

    add quick_picks.py

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,8 @@
+"""week 4 prac: quick pics
+produces a lines based on user input which show, ordered unique (per line) random numbers"""
+
+import random
+
+NUMBERS_PER_LINE = 6
+MAX_NUMBER = 45
+MIN_NUMBER = 1
