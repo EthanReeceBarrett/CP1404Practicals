4ffa531cfd5933c56a350a276d254908be04a4e8
ethan <ethanbarr64@gmail.com>
2020-09-23 10:41:45 +1000

    add car.py add taxi.py add taxi_test.py

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,18 @@
+""" tester for taxi subclass """
+
+from prac_08.taxi import Taxi
+
+
+def main():
+    """Test Taxi class."""
+    taxi_1 = Taxi("Prius 1", 100)
+    taxi_1.drive(40)
+    print(taxi_1)
+    print(taxi_1.get_fare())
+    taxi_1.start_fare()
+    taxi_1.drive(100)
+    print(taxi_1)
+    print(taxi_1.get_fare())
+
+
+main()
